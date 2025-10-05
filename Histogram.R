68fd00644eb7be09e42f3d27277f4a01cae8b72b
okoroste <82292664+okoroste@users.noreply.github.com>
2025-10-05 13:08:12 -0700

    Delete Histogram.R

--- Histogram.R
+++
@@ -1,7 +0,0 @@
-diabetes2<- read.csv(file="./diabetes_data2.csv", header=TRUE, sep=",")
-
-hist(diabetes2$percent, main='Histogram for Percent of Adults with Diagnosed 
-Diabetes in California Counties', breaks=seq(5,19,by=2), col="dodgerblue", 
-xlab="", ylab="Count")
-   
-
