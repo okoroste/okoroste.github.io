74c38c01e827bb168d622079e984d2c459cc30b8
okoroste <82292664+okoroste@users.noreply.github.com>
2025-10-05 13:07:37 -0700

    Delete ExactBinomialTest_CI.R

--- ExactBinomialTest_CI.R
+++
@@ -1,9 +0,0 @@
-#exact binomial test
-binom.test(725, 10000, p=0.07, alternative="greater")
-#options are alternative=c("two.sided", "greater", "less")
-
-#exact binomial confidence interval
-binom.test(725, 10000, conf.level = 0.9)
-
-#approximate test
-prop.test(725, 10000, conf.level=0.9)
